b5df2a1f6deea0a6853e1d014706e978a7cf4de4
chenyingbo <cybqzwork@163.com>
2019-11-24 18:34:34 +0800

    修改图片路径

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -2,22 +2,22 @@
     <Carousel autoplay loop>
         <CarouselItem>
             <div>
-                <img src="@/static/image/banner/banner01.jpg" alt="">
+                <img src="@/assets/images/banner/banner01.jpg" alt="">
             </div>
         </CarouselItem>
         <CarouselItem>
             <div>
-                <img src="@/static/image/banner/banner02.jpg" alt="">
+                <img src="@/assets/images/banner/banner02.jpg" alt="">
             </div>
         </CarouselItem>
         <CarouselItem>
             <div>
-                <img src="@/static/image/banner/banner01.jpg" alt="">
+                <img src="@/assets/images/banner/banner01.jpg" alt="">
             </div>
         </CarouselItem>
         <CarouselItem>
             <div>
-                <img src="@/static/image/banner/banner02.jpg" alt="">
+                <img src="@/assets/images/banner/banner02.jpg" alt="">
             </div>
         </CarouselItem>
     </Carousel>
