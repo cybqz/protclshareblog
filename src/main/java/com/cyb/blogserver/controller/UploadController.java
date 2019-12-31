2a41a3ff5ed2c024316508c1ee2645f61b115bf3
cybqzwork <cybqzwork@163.com>
2019-12-31 18:33:32 +0800

    接口联调20191231

--- src/main/java/com/cyb/blogserver/controller/UploadController.java
+++ src/main/java/com/cyb/blogserver/controller/UploadController.java
@@ -30,28 +30,81 @@ public class UploadController {
 
 	@PostMapping("/teclearning")
 	@ResponseBody
-	public Tips upload(@RequestParam("file") MultipartFile file) {
+	public Tips teclearning(@RequestParam("file") MultipartFile file) {
 
 		Tips tips = new Tips("文件上传失败", false, true);
-		if (StringUtils.isEmpty(IMG_PATH) || null == file || file.isEmpty()) {
+		if (isEmpty(file)) {
 			return tips;
 		}
+		String savePath = "image/teclearning/" + getSavePaht(file);
+		boolean save = saveFile(savePath, file);
+		if(save){
+			tips = new Tips("文件上传成功", false, true);
+			tips.setData(savePath);
+		}
+		return tips;
+	}
+
+	@PostMapping("/chapter")
+	@ResponseBody
+	public Tips chapter(@RequestParam("file") MultipartFile file) {
+
+		Tips tips = new Tips("文件上传失败", false, true);
+		if (isEmpty(file)) {
+			return tips;
+		}
+		String savePath = "image/chapter/" + getSavePaht(file);
+		boolean save = saveFile(savePath, file);
+		if(save){
+			tips = new Tips("文件上传成功", false, true);
+			tips.setData(savePath);
+		}
+		return tips;
+	}
+
+	/**
+	 * 获取文件保存路径
+	 * @param file
+	 * @return
+	 */
+	private String getSavePaht(MultipartFile file){
 		String fileName = file.getOriginalFilename();
 		String fileType = MyUtils.getFileTypeByFullName(fileName);
-		String savePath = "image/teclearning/" + MyUtils.getPrimaryKey()+fileType;
-		try {
+		String savePath =  MyUtils.getPrimaryKey()+fileType;
+		return savePath;
+	}
+
+	/**
+	 * 非空判断
+	 * @param file
+	 * @return
+	 */
+	private boolean isEmpty(MultipartFile file){
+		if (StringUtils.isEmpty(IMG_PATH) || null == file || file.isEmpty()) {
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	/**
+	 * 保存文件
+	 * @param savePath
+	 * @param file
+	 * @return
+	 */
+	private boolean saveFile(String savePath, MultipartFile file){
+		try{
 			File dest = new File(IMG_PATH + savePath);
 			if(dest.exists()){
 				dest.delete();
 			}
 			dest.createNewFile();
 			file.transferTo(dest);
-			tips = new Tips("文件上传成功", false, true);
-			tips.setData(savePath);
-			return tips;
-		} catch (IOException e) {
+			return true;
+		}catch (Exception e){
 			e.printStackTrace();
-			return tips;
+			return false;
 		}
 	}
 }
